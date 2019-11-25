a6b57eb9bd186896abf1fccafe349d3be7502255
MobRecordz <zenkovskiy.evgeniy@gmail.com>
2019-11-25 14:24:54 +0300

    2 files modified, 1 deleted

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # typescript-hjucaa
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-hjucaa)
+
+For some reason do not work imports :(
--- index.ts
+++ index.ts
@@ -8,11 +8,9 @@ export interface Coords2d {
 
 class CanvasApi {
 
-  element: HTMLCanvasElement
   context: CanvasRenderingContext2D
 
   constructor(public el: HTMLCanvasElement) {
-    this.element = el
 
     if (el.getContext)
       this.context = el.getContext('2d');
@@ -23,6 +21,7 @@ class CanvasApi {
 
     ctx.beginPath()
     ctx.moveTo(coords[0].x, coords[0].y)
+    ctx.lineWidth = 10
 
     coords.forEach((coord, index) => {
       if (index !== 0) {
@@ -36,10 +35,16 @@ class CanvasApi {
     
   }
 
-  public test(content: string) {
+  public clearCanvas() {
+    this.context.clearRect(0, 0, this.el.width, this.el.height);
+  }
+
+  public sendMessageToDisplay(content: string) {
     const display: HTMLDivElement = document.querySelector('.display')
     display.innerHTML = content
+  }
 
+  private initMouseEvents(event: MouseEvent) {
     console.log(this.el)
   }
 }
@@ -47,38 +52,87 @@ class CanvasApi {
 const canvasElement: HTMLCanvasElement = document.querySelector('#canvas')
 const canvasApi = new CanvasApi(canvasElement)
 
-canvasApi.test('This is a display')
+canvasApi.sendMessageToDisplay('This is a display')
 
-let mouseButtonIsDown: boolean = false
-const coords: Coords2d[] = []
+// SNAKE
+const fps = 1000 / 60
 
-canvasApi.element.addEventListener('mousemove', (event: MouseEvent) => {
-  if (mouseButtonIsDown) {
+interface SnakeBlock {
+  x: number,
+  y: number
+}
 
-    const coord: Coords2d = { 
-      x: event.offsetX, 
-      y: event.offsetY 
+const snakeHead: Coords2d = { x: 10, y: 10 }
+const snake: Coords2d[] = [snakeHead]
+
+let direction: number = 0 // 0 - up, 1 - right, 2 - down, 3 - left
+
+// Starter snake body
+const item: Coords2d = {
+  x: 11, y: 10
+}
+snake.push({ ...item })
+item.x = 12
+snake.push({ ...item })
+
+// const render = setInterval(snakeRender, fps)
+
+function snakeRender() {
+  canvasApi.clearCanvas()
+  snakeMove()
+  canvasApi.draw(snake)
+
+  // canvasApi.sendMessageToDisplay(`${JSON.stringify(snakeTail)}`)
+}
+
+function snakeMove() {
+  const { width, height } = canvasApi.el
+
+  for (const item of snake) {
+
+    switch(direction) {
+      case 0: item.y--; break;
+      case 1: item.x--; break;
+      case 2: item.y++; break;
+      case 3: item.x--; break;
     }
 
-    coords.push(coord)
-
-    canvasApi.draw(coords)
   }
-  
+}
 
-  canvasApi.test(`${event.offsetX}`)
-})
+// --------------------
+// TO DRAW WITH A MOUSE 
+// --------------------
+// let mouseButtonIsDown: boolean = false
+// let coords: Coords2d[] = []
 
-canvasApi.element.addEventListener('mousedown', () => {
-  mouseButtonIsDown = true
-})
+// canvasApi.el.addEventListener('mousemove', (event: MouseEvent) => {
+//   if (mouseButtonIsDown) {
 
-canvasApi.element.addEventListener('mouseup', () => {
-  mouseButtonIsDown = false
-})
+//     const coord: Coords2d = { 
+//       x: event.offsetX, 
+//       y: event.offsetY 
+//     }
 
+//     coords.push(coord)
+//     canvasApi.draw(coords)
+//   }
+
+//   // canvasApi.sendMessageToDisplay(`${event.offsetX}`)
+// })
+
+// canvasApi.el.addEventListener('mousedown', () => {
+//   mouseButtonIsDown = true
+// })
+
+// canvasApi.el.addEventListener('mouseup', () => {
+//   mouseButtonIsDown = false
+//   coords = []
+// })
+
+// --------------------------------------------
 // THIS CODE DRAW LINE FROM RANDOM ARRAY COORDS
-
+// --------------------------------------------
 // const coord: Coords2d = {
 //   x: 0,
 //   y: 0
